3e698af0ae6189c10ae23f0958adda5ef3dc0e68
Trisjdc <trisjdc@gmail.com>
2015-09-15 21:55:27 +0100

    Handle warden data request packets

---
+++ Client/World/Network/PacketHandlers/WardenHandler.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections.Generic;
+
+namespace Client.World.Network
+{
+    public partial class WorldSocket
+    {
+        [PacketHandler(WorldCommand.SMSG_WARDEN_DATA)]
+        void HandleWardenDataResponse(InPacket packet)
+        {
+            // flawed out packet, needs to be filled with sha1 checksum and warden data
+            OutPacket response = new OutPacket(WorldCommand.CMSG_WARDEN_DATA);
+            Game.SendPacket(response);
+        }
+    }
+}
